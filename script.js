241751421b048f4bd7cf83c316fd3bf9747e8c56
Jess <thisisjessreynolds@gmail.com>
2019-03-14 19:57:12 +0000

    hero header

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var Chapter = 1;
+var Book = 100;
+
+var littlewomen = (Book > Chapter);
+console.log(littlewomen);
+
+while(Book > Chapter) {
+    console.log ("Reading is fun")
+    Chapter=Chapter+1;
+}
+
+console.log (littlewomen == 102);
